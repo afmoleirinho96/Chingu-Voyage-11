a4af62159866ef3afb953757b18a72123b348ec9
afmoleirinho96 <andrewacm25@gmail.com>
2019-08-24 15:42:10 +0100

    first commit with minimum functionalities

---
+++ features.js
@@ -0,0 +1,200 @@
+var mainjson = {
+  "id": "place",
+  "features": [{
+      "type": "Feature",
+      "properties": {},
+      "city": "Vila Real",
+      "geometry": {
+        "coordinates": [
+          -7.762314,
+          41.294708
+        ],
+        "type": "Point"
+      },
+      "id": 1
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Leiria",
+      "geometry": {
+        "coordinates": [
+          -8.795901,
+          39.748322
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    },
+    {
+      "type": "Feature",
+      "city": "Viseu",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -7.91665,
+          40.6627
+        ],
+        "type": "Point"
+      },
+      "id": 3
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Santarén",
+      "geometry": {
+        "coordinates": [
+          -8.683177,
+          39.237131
+        ],
+        "type": "Point",
+      },
+      "id": 4
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Castelo Branco",
+      "geometry": {
+        "coordinates": [
+          -7.495732,
+          39.830977
+        ],
+        "type": "Point"
+      },
+      "id": 5
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Évora",
+      "geometry": {
+        "coordinates": [
+          -7.913769,
+          38.562951
+        ],
+        "type": "Point"
+      },
+      "id": 6
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Lisboa",
+      "geometry": {
+        "coordinates": [
+          -9.156221,
+          38.69652
+        ],
+        "type": "Point"
+      },
+      "id": 7
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Porto",
+      "geometry": {
+        "coordinates": [
+          -8.6127,
+          41.15012
+        ],
+        "type": "Point"
+      },
+      "id": 15
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Aveiro",
+      "geometry": {
+        "coordinates": [
+          -8.650918,
+          40.64496
+        ],
+        "type": "Point"
+      },
+      "id": 8
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Beja",
+      "geometry": {
+        "coordinates": [
+          -7.867216,
+          38.019625
+        ],
+        "type": "Point"
+      },
+      "id": 9
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Bragança",
+      "geometry": {
+        "coordinates": [
+          -6.733403,
+          41.812683
+        ],
+        "type": "Point"
+      },
+      "id": 10
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Braga",
+      "geometry": {
+        "coordinates": [
+          -8.389014,
+          41.554214
+        ],
+        "type": "Point"
+      },
+      "id": 11
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Faro",
+      "geometry": {
+        "coordinates": [
+          -7.941307,
+          37.007002
+        ],
+        "type": "Point"
+      },
+      "id": 12
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Guarda",
+      "geometry": {
+        "coordinates": [
+          -7.261889,
+          40.545524
+        ],
+        "type": "Point"
+      },
+      "id": 13
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "city": "Coimbra",
+      "geometry": {
+        "coordinates": [
+          -8.412398,
+          40.214196
+        ],
+        "type": "Point"
+      },
+      "id": 14
+    }
+  ],
+  "type": "FeatureCollection"
+}
